a9297fc849e63a023201f8441a2a2dd831a81f82
Adam Palmer <theadampalmer@gmail.com>
2020-07-16 14:26:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,14 @@
 class Dog
-  def 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+fido = Dog.new
+fido.name = "Fido"
+ 
+puts fido.bark
